59b03f19bb401c79381a1ed2a49de735d52b5606
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-07-04 11:16:24 -0300

    Criando projeto no XCode

---
+++ HappyNews/HappyNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyNews
+//
+//  Created by Rafael Melo on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
